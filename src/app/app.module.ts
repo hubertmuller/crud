02aba7e789fd5c00e48a040e23e78014d496443c
Hubert Muller <hubert@sportport.eu>
2021-05-30 16:04:06 +0200

    poczatkowy formularz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { DetaleComponent } from './detale/detale.component';
 import { FormularzComponent } from './formularz/formularz.component';
 import { UsunComponent } from './usun/usun.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/formularz/formularz.component.ts
+++ src/app/formularz/formularz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-formularz',
@@ -7,12 +8,23 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 })
 export class FormularzComponent implements OnInit, OnDestroy {
 
+  public forma:FormGroup = new FormGroup( {
+    imie: new FormControl('Adam', {validators: [], updateOn: "change"}),
+    nazwisko: new FormControl('Kowalski', {validators: [], updateOn: "change"})
+  }); 
+
+
   constructor() {
     console.log('konstruktor');
   }
 
   ngOnInit(): void {
     console.log('dziendobry');
+    this.forma.valueChanges.subscribe( (val) => {
+      if (val.imie === 'Hubert') {
+        console.log('hubert');
+      }
+    });
   }
 
   ngOnDestroy(): void {
